7f6fb69070acfad4f041c85262eb54bc065e3ec8
Serge Medvedev <thismoment.main@gmail.com>
2020-06-07 01:46:46 +0300

    chore(validator): folder structure is tidied up

--- validator/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COMPOSE_FILE=/opt/freeton-toolbox/validator/docker-compose.yml
-
-docker-compose -f "$COMPOSE_FILE" kill -s SIGTERM freeton-validator-dev
-docker-compose -f "$COMPOSE_FILE" up -d freeton-validator-dev
-
