57a9b6d1b40c3f6127c70902b1726a6104092f05
Serge Medvedev <thismoment.main@gmail.com>
2021-02-16 03:09:27 +0200

    feat(modern): manual console configuration is no more

--- validator/modern/entrypoint.sh
+++ validator/modern/entrypoint.sh
@@ -12,9 +12,9 @@ function configure() {
 
     curl -sS 'https://raw.githubusercontent.com/tonlabs/rustnet.ton.dev/main/docker-compose/ton-node/configs/default_config.json' -o "$CONFIGS_DIR/default_config.json"
 
-    MODIFIED=$(mktemp)
-    jq --argjson cport "$CONSOLE_PORT" '.control_server_port = $cport' "$CONFIGS_DIR/default_config.json" > "$MODIFIED" \
-        && mv "$MODIFIED" "$CONFIGS_DIR/default_config.json"
+    DEFAULT_CONFIG_JSON=$(mktemp)
+    jq --argjson cport "$CONSOLE_PORT" '.control_server_port = $cport' "$CONFIGS_DIR/default_config.json" > "$DEFAULT_CONFIG_JSON" \
+        && mv "$DEFAULT_CONFIG_JSON" "$CONFIGS_DIR/default_config.json"
 
     curl -sS 'https://raw.githubusercontent.com/tonlabs/rustnet.ton.dev/main/configs/ton-global.config.json' -o "$CONFIGS_DIR/ton-global.config.json"
 }
@@ -22,7 +22,35 @@ function configure() {
 function run() {
     ARGS=( '--configs' "$CONFIGS_DIR" )
 
-    [ ! -f "$CONFIGS_DIR/console_config.json" ] && [ -n "$CONSOLE_KEY" ] && ARGS+=( '--ckey' "$CONSOLE_KEY" )
+    if [ ! -f "$CONFIGS_DIR/console_config.json" ]; then
+        CONSOLE_KEY_INFO=$(mktemp)
+        keygen > "$CONSOLE_KEY_INFO"
+
+        CONSOLE_KEY=$(jq -r '{ type_id: 1209251014, pub_key: .public.pub_key }' "$CONSOLE_KEY_INFO")
+        ton_node "${ARGS[@]}" --ckey "$CONSOLE_KEY" 2>&1 &
+
+        TON_NODE_PID=$!
+
+        while sleep 5s; do
+            if [ -f "$CONFIGS_DIR/console_config.json" ]; then
+                kill "$TON_NODE_PID" 2> /dev/null
+                break
+            fi
+        done
+
+        CONSOLE_CONFIG_JSON=$(mktemp)
+        jq -s '.[0] as $k | .[1] | (.client_key = { type_id: 1209251014, pub_key: $k.public.pub_key })' \
+            "$CONSOLE_KEY_INFO" \
+            "$CONFIGS_DIR/console_config.json" > "$CONSOLE_CONFIG_JSON"
+        mv "$CONSOLE_CONFIG_JSON" "$CONFIGS_DIR/console_config.json"
+        SERVER_ADDR=$(printf '%s:%u' "$(curl -s eth0.me)" "$CONSOLE_PORT")
+        jq -s --arg addr "$SERVER_ADDR" '.[0] as $k | { config: .[1] | (.client_key = { type_id: 1209251014, pvt_key: $k.private.pvt_key }) | (.server_address = $addr) }' \
+            "$CONSOLE_KEY_INFO" \
+            "$CONFIGS_DIR/console_config.json" > "$CONFIGS_DIR/console.json"
+
+        echo 'INFO: console configuration completed - restarting the node...'
+        cat "$CONFIGS_DIR/console.json"
+    fi
 
     exec ton_node "${ARGS[@]}" 2>&1
 }
